5f83c385b375325c56052061085ef7d11bd18126
Khoi Hoang <hello@koicha.dev>
2020-07-02 21:35:04 +0200

    Add: :sparkles: Added CharacerGrid Component and displaying name of the characters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header.component.jsx';
+import CharacterGrid from './components/characters/CharacterGrid.component.jsx';
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,23 @@ const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // To make request
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?`);
+
+      console.log(result.data);
+      setItems(result.data)
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, []); // 2nd param: needs to have dependencies when we use filter query
 
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
