8ef3b2e87dbb20d4202753501739b4dd6a3119a5
Khoi Hoang <hello@koicha.dev>
2020-07-02 21:18:11 +0200

    Add: :spakles: Added the useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/ui/Header.component.jsx';
 import './App.css';
 
 const App = () => {
+  // 1st index: going to represent the data
+  // 1nd index: changing/manipualting the state
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+
   return (
     <div className="container">
       <Header />
